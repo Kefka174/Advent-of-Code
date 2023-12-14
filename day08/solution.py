71d3f330b0ffb4b4a08cd302a98bb54ef7e381be
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-12-14 16:10:20 -0600

    Added solution to Day 8 challenge 2

--- day08/solution.py
+++ day08/solution.py
@@ -2,25 +2,47 @@ from typing import List, Dict, Tuple
 from collections import deque
 
 def part_one(inputData: str) -> int:
-    directionDeque, _, *nodeMaps = inputData.split('\n')
-    directionDeque = deque(directionDeque)
+    directions, _, *nodeMaps = inputData.split('\n')
+    nodeMaps = nodeStringsToDict(nodeMaps)
+    return advanceNodesToEnd("AAA", directions, nodeMaps)
+
+def part_two(inputData: str) -> int:
+    directions, _, *nodeMaps = inputData.split('\n')
     nodeMaps = nodeStringsToDict(nodeMaps)
 
-    currentNode, stepCounter = "AAA", 0
-    while currentNode != "ZZZ":
-        if directionDeque[0] == 'L':
-            currentNode = nodeMaps[currentNode][0]
-        else:
-            currentNode = nodeMaps[currentNode][1]
-        stepCounter += 1
-        directionDeque.rotate(-1)
-    return stepCounter
+    currentNodes = []
+    for node in nodeMaps.keys():
+        if node[2] == 'A':
+            currentNodes.append(node)
+    
+    return advanceNodesToEnd(currentNodes, directions, nodeMaps)
 
 
 def nodeStringsToDict(nodeStringList: List[str]) -> Dict[str, Tuple[str, str]]:
     return {node.strip(): tuple(tup.strip('()').split(", ")) 
             for node, tup in (line.split(" = ") for line in nodeStringList)}
+
+def advanceNodesToEnd(nodes: str | List[str], directions: str, 
+                      nodeMaps: Dict[str, Tuple[str, str]]) -> int:
+    if type(nodes) == str:
+        nodes = [nodes]
+    directionDeque = deque(directions)
     
+    stepCounter, nodesAtFinish = 0, 0
+    while nodesAtFinish != len(nodes):
+        nodesAtFinish = 0
+        for i in range(len(nodes)):
+            if directionDeque[0] == 'L':
+                nodes[i] = nodeMaps[nodes[i]][0]
+            else:
+                nodes[i] = nodeMaps[nodes[i]][1]
+
+            if nodes[i][2] == 'Z':
+                nodesAtFinish += 1
+        stepCounter += 1
+        directionDeque.rotate(-1)
+
+    return stepCounter
 
 ################################ TESTING #################################
 testInput1 = """RL
@@ -39,9 +61,22 @@ testInput2 = """LLR
                 BBB = (AAA, ZZZ)
                 ZZZ = (ZZZ, ZZZ)"""
 
-print("Part 1 first test input:", part_one(testInput1))
-print("Part 1 second test input:", part_one(testInput2))
+testInput3 = """LR
+
+                11A = (11B, XXX)
+                11B = (XXX, 11Z)
+                11Z = (11B, XXX)
+                22A = (22B, XXX)
+                22B = (22C, 22C)
+                22C = (22Z, 22Z)
+                22Z = (22B, 22B)
+                XXX = (XXX, XXX)"""
+
+# print("Part 1 first test input:", part_one(testInput1))
+# print("Part 1 second test input:", part_one(testInput2))
+print("Part 2 test input:", part_two(testInput3))
 
 with open("input.txt", 'r') as file:
     fileInput = file.read()
-    print("Part 1 file input:", part_one(fileInput))
+    # print("Part 1 file input:", part_one(fileInput))
+    print("Part 2 file input:", part_two(fileInput))
