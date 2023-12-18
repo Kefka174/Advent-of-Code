6c025a85facf8e9a730d1d6b4bd0789ce204271c
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2023-12-18 12:13:41 -0600

    Refactored Day 10 challenge 1

--- day10/solution.py
+++ day10/solution.py
@@ -1,49 +1,66 @@
-from typing import List, Tuple
+from typing import List, Tuple, Set
 
 def part_one(inputData: str) -> int:
     pipeMap = [[*line.strip()] for line in inputData.split('\n')]
-    start = [(rowNum, row.index('S')) for rowNum, row in enumerate(pipeMap) 
-             if 'S' in row][0]
-    
-    nextDirection = findConnectedPipe(start, pipeMap)
-    currentPipe = directionToCoords(start, nextDirection)
-    stepCount = 1
-    while currentPipe != start:
-        nextDirection = getNextDirection(pipeMap[currentPipe[0]][currentPipe[1]], nextDirection)
-        currentPipe = directionToCoords(currentPipe, nextDirection)
-        stepCount += 1
-    return stepCount // 2
+    start, connectedPipeDirections = findAndReplaceStart(pipeMap)
+    return len(getConnectedPipes(start, connectedPipeDirections, pipeMap)) // 2
 
 
-def findConnectedPipe(startPoint: Tuple[int, int], pipeMap: List[List[str]]) -> str:
-    direction, dirIndex = ["right", "down", "left", "up"], 0
-    relativeCoords = (0, 1)
-    for _ in range(4):
-        neighbor = (startPoint[0] + relativeCoords[0], startPoint[1] + relativeCoords[1])
+DIRECTIONS = ["right", "down", "left", "up"]
+DIRECTION_INVERSES = {"right": "left", "down": "up", "left": "right", "up": "down"}
+PIPE_DIRECTIONS = {'|': ("up", "down"), '-': ("left", "right"), 'L': ("up", "right"), 
+                  'J': ("up", "left"), '7': ("left", "down"), 'F': ("right", "down")}
+
+# Finds the start 'S' in a pipeMap and replaces it with the appropriate pipe
+# Returns the starting coordinates and the directions of its connected pipes
+def findAndReplaceStart(pipeMap: List[List[str]]) -> Tuple[int, int]:
+    startCoords = [(rowNum, row.index('S')) for rowNum, row in enumerate(pipeMap) 
+                   if 'S' in row][0]
+    connectedPipeDirections = set()
+    relativeCoords, directionIndex = (0, 1), 0
+    for _ in range(4): # coupled to directions order [right, down, left, up]
+        neighbor = (startCoords[0] + relativeCoords[0], startCoords[1] + relativeCoords[1])
         neighborIsInBounds = (neighbor[0] >= 0 and neighbor[0] < len(pipeMap) 
                               and neighbor[1] >= 0 and neighbor[1] < len(pipeMap[0]))
-        nextDirection = getNextDirection(pipeMap[neighbor[0]][neighbor[1]], direction[dirIndex])
-        
+        nextDirection = getNextDirection(pipeMap[neighbor[0]][neighbor[1]], 
+                                         DIRECTIONS[directionIndex])
+
         if neighborIsInBounds and nextDirection is not None:
-            return direction[dirIndex]
+            connectedPipeDirections.add(DIRECTIONS[directionIndex])
 
         relativeCoords = (relativeCoords[1], -relativeCoords[0])
-        dirIndex += 1
+        directionIndex += 1
 
-def getNextDirection(pipe: str, directionTraveledIn: str) -> str:
-    pipeDirections = {'|': ("up", "down"), '-': ("left", "right"), 
-                      'L': ("up", "right"), 'J': ("up", "left"), 
-                      '7': ("left", "down"), 'F': ("right", "down")}
-    if pipe not in pipeDirections: return None
-    invertDirection = {"right": "left", "left": "right", "up": "down", "down": "up"}
-    directionApproachedFrom = invertDirection[directionTraveledIn]
-
-    if pipeDirections[pipe][0] == directionApproachedFrom:
-        return pipeDirections[pipe][1]
-    else:
-        return pipeDirections[pipe][0]
+    for pipe in PIPE_DIRECTIONS.items():
+        if set(pipe[1]) == connectedPipeDirections:
+            pipeMap[startCoords[0]][startCoords[1]] = pipe[0]
     
-def directionToCoords(startCoords: Tuple[int, int], direction: str) -> Tuple[int, int]:
+    return startCoords, list(connectedPipeDirections)
+
+# Returns a list of the pipes in a pipe loop
+def getConnectedPipes(start: Tuple[int, int], connectedPipeDirections: List[str], 
+                      pipeMap: List[List[str]]) -> List[Tuple[int, int]]:
+    pipesInLoop = [start]
+    nextDirection = connectedPipeDirections[0]
+    currentPipe = getCoordsFromDirection(start, nextDirection)
+    while currentPipe != start:
+        pipesInLoop.append(currentPipe)
+        nextDirection = getNextDirection(pipeMap[currentPipe[0]][currentPipe[1]], nextDirection)
+        currentPipe = getCoordsFromDirection(currentPipe, nextDirection)
+    return pipesInLoop
+
+# Returns the Direction a pipe leads given the direction used to travel to that pipe
+def getNextDirection(pipe: str, directionTraveledIn: str) -> str:
+    if pipe in PIPE_DIRECTIONS:
+        directionApproachedFrom = DIRECTION_INVERSES[directionTraveledIn]
+
+        if PIPE_DIRECTIONS[pipe][0] == directionApproachedFrom:
+            return PIPE_DIRECTIONS[pipe][1]
+        elif PIPE_DIRECTIONS[pipe][1] == directionApproachedFrom:
+            return PIPE_DIRECTIONS[pipe][0]
+    return None
+    
+def getCoordsFromDirection(startCoords: Tuple[int, int], direction: str) -> Tuple[int, int]:
     match direction:
         case "up":
             return (startCoords[0] - 1, startCoords[1])
@@ -67,8 +84,8 @@ testInput2 = """7-F7-
                 |F--J
                 LJ.LJ"""
 
-print("Part 1 first test input:", part_one(testInput1))
-print("Part 1 second test input:", part_one(testInput2))
+print("Part 1 first test input:", part_one(testInput1)) # expect 4
+print("Part 1 second test input:", part_one(testInput2)) # expect 8
 
 with open("input.txt", 'r') as file:
     fileInput = file.read()
